059b322f44f0193db47070a649418af9f87dd19b
Fihra <fabrofabian@gmail.com>
2021-05-11 21:35:08 -1000

    added red dot on filling the cart

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
-import {SiAudiomack} from 'react-icons/si'
+import {SiAudiomack} from 'react-icons/si';
+import { ProductContext } from './ProductContext'; 
 
 const Navbar = () => {
+    const { productsData } = useContext(ProductContext);
+    console.log(productsData.myCart);
+
+    const showRedDot = () => {
+        return(
+            <div className="red-dot"></div>
+        )
+    }
+
     return(
         <div>
             <SiAudiomack/>
@@ -11,7 +21,10 @@ const Navbar = () => {
                 <li><Link to="/vsts">Virtual Instruments</Link></li>
                 <li><Link to="/contact">Contact</Link></li>
             </ul>
+            <div>
             <span style={{border: "1px solid black"}}><Link to="/cart">Cart</Link></span>
+            {productsData.myCart !== "" && productsData.myCart.length >= 1 ? showRedDot() : "" }
+            </div>
         </div>
     )
 }
