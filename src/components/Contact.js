8f15587c333d3bfe95cba38e72414f00c2260bd6
Fihra <fabrofabian@gmail.com>
2021-05-16 18:17:47 -1000

    deleted forms file and deleted commented out code

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import emailjs from 'emailjs-com';
 import { useForm } from 'react-hook-form';
-// import { Forms } from './Forms';
 
 const Contact = () => {
-    const { register, handleSubmit, watch, errors } = useForm();
+    const { register, handleSubmit, watch, errors, reset } = useForm();
 
     const onSubmit = (data) => {
-        console.log(data);
 
         emailjs.send('SERVICE', 'test_template', data, 'user_TJIpDPl9Uo4BzRuUyOBau')
           .then((result) => {
@@ -16,28 +14,14 @@ const Contact = () => {
           }, (error) => {
               console.log(error.text);
           });
-        // data.reset();
+        reset();
     }
-
-    // const createFormInputs = () => {
-    //     console.log(Forms);
-    //     return Forms.map((formInput, i) => {
-    //         return (
-    //             <div key={i}>
-    //                 <label>{formInput.label}</label>
-    //                 <input defaultValue={formInput.defaultValue} type={formInput.type} name={formInput.name} {...register(formInput.name, formInput.requirements)}/>
-    //             </div>
-    //         )
-    //     })
-    // }
-
 
     return (
         <div>
             <form onSubmit={handleSubmit(onSubmit)}>
             <fieldset>
                 <legend>Contact Form</legend>
-                {/* {createFormInputs()} */}
                 <label>Name</label>
                 <input defaultValue="Testing" type="text" name="username" {...register("username")}/>
                 <label>Subject</label>
@@ -46,7 +30,6 @@ const Contact = () => {
                 <input defaultValue="example@email.com" type="email" name="email" {...register("email")}/>
                 <label>Message</label>
                 <textarea defaultValue="Have any questions?" name="message" {...register("message")}/>
-
                 <input type="submit" value="Submitting"></input>
             </fieldset>
             </form>
