b748dae150af68a032526dd8147e505e1bc88461
Fihra <fabrofabian@gmail.com>
2021-05-16 21:29:08 -1000

    added material ui and changed the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,13 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
 import {SiAudiomack} from 'react-icons/si';
 import { ProductContext } from './ProductContext'; 
+import { makeStyles } from '@material-ui/core/styles';
+import {Paper, Tabs, Tab} from '@material-ui/core';
 
 const Navbar = () => {
+    const [value, setValue] = useState();
+
     const { productsData } = useContext(ProductContext);
 
     const showRedDot = () => {
@@ -13,18 +17,28 @@ const Navbar = () => {
     }
 
     return(
-        <div>
+        <Paper>
             <SiAudiomack/>
-            <ul className="nav-bar">
+            {/* <ul className="nav-bar">
                 <li><Link to="/about">About</Link></li>
                 <li><Link to="/products">Products</Link></li>
                 <li><Link to="/contact">Contact</Link></li>
-            </ul>
-            <div>
+            </ul> */}
+            <Tabs
+                value={value}
+                onChange={() => setValue(value)}
+                centered
+            >
+                <Tab label="About" to="/about" component={Link} value={0}/>
+                <Tab label="Products" to="/products" component={Link} value={1}/>
+                <Tab label="Contact" to="/contact" component={Link}  value={2}/>
+                <Tab label="Cart" to="/cart" component={Link}  value={3}/>
+            </Tabs>
+            {/* <div>
             <span style={{border: "1px solid black"}}><Link to="/cart">Cart</Link></span>
             {productsData.myCart !== "" && productsData.myCart.length >= 1 ? showRedDot() : "" }
-            </div>
-        </div>
+            </div> */}
+        </Paper>
     )
 }
 
